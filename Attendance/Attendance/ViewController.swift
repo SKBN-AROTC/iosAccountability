c597b53b4d04c760b0dab9efd9263fe17cc82af9
William Chu <willcty@gmail.com>
2022-10-04 15:23:42 -0400

    Initial GUI and loc debug added

--- Attendance/Attendance/ViewController.swift
+++ Attendance/Attendance/ViewController.swift
@@ -6,12 +6,53 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // GUI Elements
+    let locLabel = UILabel()
+    let timeLabel = UILabel()
+    let button = UIButton()
+    
+    // Date time
+    let date = Date()
+    
+    // GPS
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+
+    }
+    
+    @IBAction func buttonClicked() {
+        // print current time
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        print("time = \(hour):\(minutes)")
+        timeLabel.text = "\(hour):\(minutes)" // Debug purposes
+        
+        // print current gps
+        guard let locValue: CLLocationCoordinate2D = locationManager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        locLabel.text = "\(locValue.latitude) \(locValue.longitude)" // Debug purposes
+        
+        
     }
 
 
